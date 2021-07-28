76948526a25966433c2cd234aa8031dbe0477e42
GrasiaMeliolla <74174423+GrasiaMeliolla@users.noreply.github.com>
2021-07-28 21:25:58 +0800

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -45,14 +45,14 @@ class _WebViewExampleState extends State<WebViewExample> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('ELC'),
+    //   appBar: AppBar(
+       //  title: const Text('ELC'),
         // This drop down menu demonstrates that Flutter widgets can be shown over the web view.
-        actions: <Widget>[
-          NavigationControls(_controller.future),
-          SampleMenu(_controller.future),
-        ],
-      ),
+      //   actions: <Widget>[
+       //    NavigationControls(_controller.future),
+     //      SampleMenu(_controller.future),
+     //    ],
+    //   ),
       // We're using a Builder here so we have a context that is below the Scaffold
       // to allow calling Scaffold.of(context) so we can show a snackbar.
       body: Builder(builder: (BuildContext context) {
