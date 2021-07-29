913a778fe73722abed1d4ae3e0757e09832f7d8d
GrasiaMeliolla <74174423+GrasiaMeliolla@users.noreply.github.com>
2021-07-29 10:17:35 +0800

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -45,14 +45,14 @@ class _WebViewExampleState extends State<WebViewExample> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-    //   appBar: AppBar(
-       //  title: const Text('ELC'),
+      //appBar: AppBar(
+        //title: const Text('ELC'),
         // This drop down menu demonstrates that Flutter widgets can be shown over the web view.
-      //   actions: <Widget>[
-       //    NavigationControls(_controller.future),
-     //      SampleMenu(_controller.future),
-     //    ],
-    //   ),
+       // actions: <Widget>[
+         // NavigationControls(_controller.future),
+          //SampleMenu(_controller.future),
+        //],
+      //),
       // We're using a Builder here so we have a context that is below the Scaffold
       // to allow calling Scaffold.of(context) so we can show a snackbar.
       body: Builder(builder: (BuildContext context) {
@@ -85,7 +85,7 @@ class _WebViewExampleState extends State<WebViewExample> {
           gestureNavigationEnabled: true,
         );
       }),
-      floatingActionButton: favoriteButton(),
+     // floatingActionButton: favoriteButton(),
     );
   }
 
@@ -100,26 +100,26 @@ class _WebViewExampleState extends State<WebViewExample> {
         });
   }
 
-  Widget favoriteButton() {
-    return FutureBuilder<WebViewController>(
-        future: _controller.future,
-        builder: (BuildContext context,
-            AsyncSnapshot<WebViewController> controller) {
-          if (controller.hasData) {
-            return FloatingActionButton(
-              onPressed: () async {
-                final String url = (await controller.data!.currentUrl())!;
-                // ignore: deprecated_member_use
-                Scaffold.of(context).showSnackBar(
-                  SnackBar(content: Text('Favorited $url')),
-                );
-              },
-              child: const Icon(Icons.favorite),
-            );
-          }
-          return Container();
-        });
-  }
+  //Widget favoriteButton() {
+    //return FutureBuilder<WebViewController>(
+      //  future: _controller.future,
+        //builder: (BuildContext context,
+//            AsyncSnapshot<WebViewController> controller) {
+  //        if (controller.hasData) {
+    //        return FloatingActionButton(
+      //        onPressed: () async {
+        //        final String url = (await controller.data!.currentUrl())!;
+              //  // ignore: deprecated_member_use
+          //      Scaffold.of(context).showSnackBar(
+            //      SnackBar(content: Text('Favorited $url')),
+                //);
+              //},
+             // child: const Icon(Icons.favorite),
+           // );
+         // }
+         // return Container();
+       // });
+ // }
 }
 
 enum MenuOptions {
